9391a9f9332656d618970e059efb9e73c7caeae2
yosefsahle <yosefshale48@gmail.com>
2026-05-14 05:57:36 -0300

    about page and floating and animation css

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,43 @@
 <template>
-    <div class="bg-cover bg-center bg-[url('../../assets/bg-dots.jpg')]">Hello About</div>
+    <div class="flex flex-col md:flex-row mt-5 items-center ">
+        <div class="z-2 w-full flex flex-col justify-between gap-5 items-center md:items-start animate-container">
+            <p class="text-4xl md:text-5xl font-semibold animate-item delay-1">
+             A Little About <span class="font-bold text-[#0DB760]">ME</span>
+            </p>
+            <p class="text-sm text-[#0DB760] animate-item delay-2">💻 Software Engineer | 🎨 Graphic Designer | 🎓 AAU Alum</p>
+            <p class="text-sm md:text-md animate-item delay-3">I am a Software Engineer and Graphic Designer dedicated to the intersection of logic and aesthetics. A proud graduate of Addis Ababa University, I combine the structural discipline of engineering with a deep-seated passion for visual storytelling. Whether I’m writing clean, scalable code or crafting a brand identity from scratch, my goal is always to create seamless digital experiences</p>
+            
+        </div>
+        <div class="z-1 w-full  flex justify-center items-center animate-item-side delay-4">
+             
+               <img src="../assets/yosefanimation-white.png" alt="" class="md:w-100 md:h-100">
+        </div>
+        
+        <div class="z-0 overflow-hidden absolute w-full md:w-5/6 h-full">
+            <div class="top-30 left-20 absolute w-[30px] h-[30px] bg-[#3A86FF] rounded-[100%] blur-sm"></div>
+            <div class="top-50 left-20 absolute w-[30px] h-[30px] bg-[#F4C430] rounded-[100%] blur-sm"></div>
+            <!-- <div class="top-20 left-30 absolute w-[30px] h-[30px] bg-[#E63946] rounded-[100%] blur-sm"></div>
+            <div class="top-10 left-50 absolute w-[30px] h-[30px] bg-[#3A86FF] rounded-[100%] blur-sm"></div> -->
+            <div class="top-80 left-40 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div>
+            <div class="top-30 left-60 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div>
+            
+            <!-- <div class="top-5 left-90 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div>
+            <div class="top-60 left-50 absolute w-[30px] h-[30px] bg-[#F4C430] rounded-[100%] blur-sm"></div>
+            <div class="top-40 left-10 absolute w-[30px] h-[30px] bg-[#E63946] rounded-[100%] blur-sm"></div>
+            <div class="top-20 left-90 absolute w-[30px] h-[30px] bg-[#3A86FF] rounded-[100%] blur-sm"></div> -->
+            <div class="top-10 left-10 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div>
+            <div class="top-90 left-80 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div>
+
+
+            <div class="top-30 right-20 absolute w-[100px] h-[100px] bg-[#F4C430] rounded-full blur-sm float-1"></div>
+            <div class="top-40 right-80 absolute w-[100px] h-[100px] bg-[#E63946] rounded-full blur-sm float-2"></div>
+            <div class="top-20 right-60 absolute w-[100px] h-[100px] bg-[#3A86FF] rounded-full blur-sm float-3"></div>
+            <div class="top-50 right-10 absolute w-[100px] h-[100px] bg-[#0DB760] rounded-full blur-sm float-4"></div>
+            <div class="top-35 right-22 absolute w-[300px] h-[300px] bg-[#F4C430] rounded-full blur-lg float-1"></div>
+            <div class="top-90 right-70 absolute w-[100px] h-[100px] bg-[#0DB760] rounded-full blur-sm float-2"></div>
+            <div class="top-85 right-20 absolute w-[100px] h-[100px] bg-[#E63946] rounded-full blur-sm float-3"></div>
+        </div>
+
+    </div>
 
 </template>
