e068f9d909b534fe70628770c4ccd447975223c5
yosefsahle <yosefshale48@gmail.com>
2026-05-18 04:14:40 -0300

    projects page header

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -16,57 +16,72 @@
             <div class="top-20 left-90 absolute w-[30px] h-[30px] bg-[#3A86FF] rounded-[100%] blur-sm"></div>
             <!-- <div class="top-10 left-10 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div> -->
             <div class="top-90 left-80 absolute w-[30px] h-[30px] bg-[#0DB760] rounded-[100%] blur-sm"></div>
-            <div class="z-1 top-90 right-80 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-1 flex items-center ">
+            <div class="z-2 top-90 right-80 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-1 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=html" class="w-8" />
                 Html
             </div>
-            <div class="z-1 top-20 left-20 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-2 flex items-center ">
+            <div class="z-2 top-20 left-20 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-2 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=flutter" class="w-8" />
                 Flutter
             </div>
-            <div class="z-1 top-40 right-20 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-3 flex items-center ">
+            <div class="z-2 top-40 right-20 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-3 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=next" class="w-8" />
                 Nextjs
             </div>
-            <div class="z-1 top-50 left-80 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-4 flex items-center ">
+            <div class="z-2 top-50 left-80 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-4 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=vue" class="w-8" />
                 Vue.js
             </div>
-            <div class="z-1 top-85 left-60 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-5 flex items-center ">
+            <div class="z-2 top-85 left-60 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-5 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=photoshop" class="w-8" />
                 Photoshop
             </div>
-            <div class="z-1 top-80 left-0 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-6 flex items-center ">
+            <div class="z-2 top-80 left-0 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-6 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=react" class="w-8" />
                 React
             </div>
-            <div class="z-1 top-30 right-80 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-7 flex items-center ">
+            <div class="z-2 top-30 right-80 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-7 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=angular" class="w-8" />
                 Angular
             </div>
-            <div class="z-1 top-10 right-20 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-8 flex items-center ">
+            <div class="z-2 top-10 right-20 absolute bg-white rounded-md px-2 py-2 gap-5 shadow-lg float-8 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=illustrator" class="w-8" />
                 Illustrator
             </div>
-            <div class="z-1 top-10 right-120 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
+            <div class="z-2 top-10 right-120 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=illustrator" class="w-8" />
             </div>
             <div class="z-1 top-60 right-90 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=illustrator" class="w-8" />
             </div>
-            <div class="z-1 top-80 right-130 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
+            <div class="z-2 top-80 right-130 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
+                <img src="https://skillicons.dev/icons?i=figma" class="w-8" />
+            </div>
+            <div class="z-2 top-80 right-60 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=illustrator" class="w-8" />
             </div>
-            <div class="z-1 top-80 right-60 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
+            <div class="z-2 top-10 right-120 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
                 <img src="https://skillicons.dev/icons?i=illustrator" class="w-8" />
             </div>
-            <div class="z-1 top-10 right-120 absolute bg-white rounded-md p-2 gap-5 shadow-lg float-1 flex items-center ">
-                <img src="https://skillicons.dev/icons?i=illustrator" class="w-8" />
+            <div class="z-1 rotate-270 md:rotate-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md p-2 flex items-center w-full items-center justify-center">
+                <p class="text-[#0DB760] text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">M</p>
+                <p class="text-[#0DB760] text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">Y</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">&ensp;</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">P</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">R</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">O</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">J</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">E</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">C</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">T</p>
+                <p class="text-9xl font-extrabold z-1 hover:text-yellow-500 cursor-pointer hover:scale-130 transition-transform duration-300">S</p>
+
             </div>
 
 
             <div class="md:top-30 top-80 md:right-20 right-8 absolute w-[100px] h-[100px] bg-[#F4C430] rounded-full blur-sm"></div>
-            <div class="md:top-40 top-90 md:right-100 right-70 absolute w-[100px] h-[100px] bg-[#E63946] rounded-full blur-sm"></div>
+            <div class="z-2 md:top-40 top-90 md:right-100 right-70 absolute w-[100px] h-[100px] bg-[#E63946] rounded-full blur-sm"></div>
+            <div class="z-2 md:top-70 top-90 md:right-10 right-70 absolute w-[100px] h-[100px] bg-[#E63946] rounded-full blur-sm"></div>
             <div class="md:top-20 top-70 md:right-60 right-45 absolute w-[100px] h-[100px] bg-[#3A86FF] rounded-full blur-sm"></div>
             <div class="md:top-50 top-100 md:right-150 right-0 absolute w-[100px] h-[100px] bg-[#0DB760] rounded-full blur-sm"></div>
             <div class="md:top-35 top-85 md:right-22 right-10 absolute w-[300px] h-[300px] bg-[#F4C430] rounded-full blur-lg"></div>
