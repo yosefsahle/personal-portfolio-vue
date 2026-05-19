ebdb3a28ba1a9437d3dc4c003c77364768cacc50
yosefsahle <yosefshale48@gmail.com>
2026-05-19 07:32:00 -0300

    remove for now

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="z-0 overflow-hidden absolute w-full md:w-5/6 h-full">
+    <!-- <div class="z-0 overflow-hidden absolute w-full md:w-5/6 h-full">
         <div class="top-30 left-10 absolute w-[10px] h-[10px] bg-[#3A86FF] rounded-[100%] "></div>
         <div class="top-30 left-15 absolute w-[10px] h-[10px] bg-[#F4C430] rounded-[100%] "></div>
         <div class="top-30 left-20 absolute w-[10px] h-[10px] bg-[#E63946] rounded-[100%] "></div>
@@ -34,5 +34,5 @@
         <div class="top-50 left-25 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
-    </div>
+    </div> -->
 </template>
