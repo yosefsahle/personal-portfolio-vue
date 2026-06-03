c42c7f9e60e5b751157dc4dc97bea6e9e858ab4a
yosefsahle <yosefshale48@gmail.com>
2026-06-03 01:03:35 -0300

    projects page and about page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="z-0 overflow-hidden absolute w-full md:w-5/6 h-full">
+    <div class="z-0 overflow-hidden absolute w-full md:w-5/6 h-full opacity-50">
         <div class="top-30 left-10 absolute w-[10px] h-[10px] bg-[#3A86FF] rounded-[100%] "></div>
         <div class="top-30 left-15 absolute w-[10px] h-[10px] bg-[#F4C430] rounded-[100%] "></div>
         <div class="top-30 left-20 absolute w-[10px] h-[10px] bg-[#E63946] rounded-[100%] "></div>
@@ -35,5 +35,17 @@
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
     </div>
-    <h1 class="text-center">Page Not Found</h1>
+
+    
+    <div class="h-100 flex flex-col justify-center gap-20 items-center">
+    <div class="flex justify-center gap-20 items-center">
+        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-bold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">4</p>
+        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-bold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">0</p>
+        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-bold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">4</p>
+    </div>
+    <div class="flex justify-center gap-20 items-center">
+        <p class="text-3xl text-[#0DB760] font-bold scale-180  cursor-pointer transition-transform duration-300">Page Not Found</p>
+    </div>
+    </div>
+
 </template>
