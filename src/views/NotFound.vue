17ea61ba699369fa9d9b541ef530378f0b2f4ae1
yosefsahle <yosefshale48@gmail.com>
2026-06-14 08:21:38 -0300

    me page updates excersise

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="mockup-phone scale-80 sm:scale-50 absolute top-110 sm:top-80 md:top-85 lg:top-75 left-50 -translate-x-1/2 -translate-y-1/2 z-10">
+  <div class="">
+    <div class="mockup-phone scale-80 sm:scale-50 absolute top-110 sm:top-80 md:top-85 lg:top-75 left-50 sm:left-40 -translate-x-1/2 -translate-y-1/2 z-10">
   
   <div class="mockup-phone-camera"></div>
   
@@ -12,6 +13,19 @@
     ></iframe>
 
   </div>
+</div>
+<div class="sm:block absolute hidden sm:left-70  sm:top-22 mockup-window border border-base-300 min-w-[75%] h-[480px] z-2">
+  <div class="border-t border-base-300 h-100">
+    
+    <iframe
+      src="https://personal-portfolio-vue-olive.vercel.app/"
+      class="w-full h-full"
+      frameborder="0"
+      loading="lazy"
+    ></iframe>
+
+  </div>
+</div>
 </div>
     <div class="z-0 overflow-hidden absolute w-full md:w-5/6 h-full opacity-50">
         <div class="top-30 left-10 absolute w-[10px] h-[10px] bg-[#3A86FF] rounded-[100%] "></div>
@@ -49,36 +63,24 @@
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
     </div>
-
-    
-    <div class="h-100 flex flex-col justify-center gap-20 items-center  ml-80">
-    <div class="flex justify-center gap-15 items-center">
-        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">W</p>
-        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-extrabold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">A</p>
-        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">T</p>
-        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-extrabold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">C</p>
-        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-180 hover:scale-200 cursor-pointer transition-transform duration-300">H</p>
+    <hr class="mt-130">
+    <div class="mt-15 h-100 flex flex-col justify-center gap-20 items-center ml-5">
+    <div class="flex justify-center gap-30 items-center">
+        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">W</p>
+        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">A</p>
+        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">T</p>
+        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">C</p>
+        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">H</p>
     </div>
-    <div class="flex justify-center gap-20 items-center tracking-[7px]">
-        <p class="text-3xl text-[#0DB760] font-thin scale-180  cursor-pointer transition-transform duration-300">With Scroll Vision here</p>
+    <div class="flex justify-center gap-20 items-center tracking-[12px] mt-5">
+        <p class="text-3xl text-[#0DB760] font-thin scale-180  cursor-pointer transition-transform duration-300">WITH SCROLL VISION HERE</p>
     </div>
     </div>
     <div class="flex mt-30 gap-20 mb-10">
       <div class="max-w-[20%]">
       <p class="text-[#0DB760] text-9xl font-extrabold">WEB</p>
     </div>
-    <div class="mockup-window border border-base-300 min-w-[80%]">
-  <div class="border-t border-base-300 h-80">
     
-    <iframe
-      src="https://personal-portfolio-vue-olive.vercel.app/"
-      class="w-full h-full"
-      frameborder="0"
-      loading="lazy"
-    ></iframe>
-
-  </div>
-</div>
     
     </div>
 </template>
