7cb0b82bee1ec62dfd6e769e5c5da4a9fa376013
yosefsahle <yosefshale48@gmail.com>
2026-06-15 06:30:04 -0300

    service page included

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -64,24 +64,92 @@
         <div class="top-50 left-30 absolute w-[10px] h-[10px] bg-[#0DB760] rounded-[100%] "></div>
     </div>
     <hr class="mt-130">
-    <div class="mt-15 h-100 flex flex-col justify-center gap-20 items-center ml-5">
-    <div class="flex justify-center gap-30 items-center">
-        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">W</p>
-        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">A</p>
-        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">T</p>
-        <p class="text-9xl text-[#0DB760]/20 hover:text-yellow-500/50 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">C</p>
-        <p class="text-9xl text-yellow-500/50 hover:text-[#0DB760]/20 font-extrabold scale-250 hover:scale-200 cursor-pointer transition-transform duration-300">H</p>
-    </div>
-    <div class="flex justify-center gap-20 items-center tracking-[12px] mt-5">
-        <p class="text-3xl text-[#0DB760] font-thin scale-180  cursor-pointer transition-transform duration-300">WITH SCROLL VISION HERE</p>
-    </div>
-    </div>
-    <div class="flex mt-30 gap-20 mb-10">
-      <div class="max-w-[20%]">
-      <p class="text-[#0DB760] text-9xl font-extrabold">WEB</p>
+    
+    <div class="mt-30 md:mt-15 h-100 flex flex-col justify-center md:gap-20 items-center">
+    <div class="text-4xl md:text-9xl font-extrabold flex justify-center gap-5 md:gap-17 items-center">
+        <p class="text-yellow-500/50 hover:text-[#0DB760]/20 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">S</p>
+        <p class="text-[#0DB760]/20 hover:text-yellow-500/50 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">E</p>
+        <p class="text-yellow-500/50 hover:text-[#0DB760]/20 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">R</p>
+        <p class="text-[#0DB760]/20 hover:text-yellow-500/50 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">V</p>
+        <p class="text-yellow-500/50 hover:text-[#0DB760]/20 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">I</p>
+        <p class="text-[#0DB760]/20 hover:text-yellow-500/50 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">C</p>
+        <p class="text-yellow-500/50 hover:text-[#0DB760]/20 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">E</p>
+        <p class="text-[#0DB760]/20 hover:text-yellow-500/50 scale-200 hover:scale-180 cursor-pointer transition-transform duration-300">S</p>
     </div>
     
-    
+    <div class="flex justify-center gap-20 items-center tracking-[5px] md:tracking-[15px] mt-5 ml-5">
+        <p class="text-lg md:text-2xl font-light md:scale-175  cursor-pointer transition-transform duration-300"><span class="text-rotate">
+  <span class="justify-items-center">
+    <span class="text-[#0DB760]">BUILD - LAUNCH - SCALE</span>
+    <span class="text-yellow-500/80">FAST - MODERN - SCALABLE</span>
+    <span class="text-[#0DB760]">WEB - MOBILE - FULL-STACK</span>
+    <span class="text-yellow-500/80">FROM IDEA TO APP.</span>
+    <span class="text-[#0DB760]">CODE THAT DELIVERS.</span>
+  </span>
+</span></p>
+    </div>
+    </div>
+    <div class="mt-5 mb-20 flex gap-5 flex-col">
+    <details class="collapse border-base-300 border">
+      <summary class="collapse-title font-semibold">💻 Web Development</summary>
+      <div class="collapse-content text-sm">
+        <ul class="list rounded-box shadow-md">
+          <li class="list-row">Modern, responsive websites</li>
+          <li class="list-row">Built with React / Next.js / Tailwind</li>
+          <li class="list-row">SEO-friendly & fast performance</li>
+        </ul>
+      </div>
+    </details>
+    <details class="collapse border-base-300 border">
+      <summary class="collapse-title font-semibold">📱 Mobile App Development</summary>
+      <div class="collapse-content text-sm">
+        <ul class="list rounded-box shadow-md">
+          <li class="list-row">Android apps using Flutter / Kotlin</li>
+          <li class="list-row">Smooth UI + high performance</li>
+          <li class="list-row">API integration & real-time features</li>
+        </ul>
+      </div>
+    </details>
+    <details class="collapse border-base-300 border">
+      <summary class="collapse-title font-semibold">🧠 Full Stack Development</summary>
+      <div class="collapse-content text-sm">
+        <ul class="list rounded-box shadow-md">
+          <li class="list-row">Frontend + Backend complete solutions</li>
+          <li class="list-row">REST APIs (NestJS / Node.js / django)</li>
+          <li class="list-row">Database design (PostgreSQL, MongoDB)</li>
+        </ul>
+      </div>
+    </details>
+    <details class="collapse border-base-300 border">
+      <summary class="collapse-title font-semibold">🎨 UI/UX Design</summary>
+      <div class="collapse-content text-sm">
+        <ul class="list rounded-box shadow-md">
+          <li class="list-row">Clean, modern mobile & web designs</li>
+          <li class="list-row">Figma prototypes</li>
+          <li class="list-row">User-focused experience</li>
+        </ul>
+      </div>
+    </details>
+    <details class="collapse border-base-300 border">
+      <summary class="collapse-title font-semibold">⚙️ Backend Development</summary>
+      <div class="collapse-content text-sm">
+        <ul class="list rounded-box shadow-md">
+          <li class="list-row">Secure & scalable APIs</li>
+          <li class="list-row">Authentication (JWT, OTP, RBAC)</li>
+          <li class="list-row">Performance optimization</li>
+        </ul>
+      </div>
+    </details>
+    <details class="collapse border-base-300 border">
+      <summary class="collapse-title font-semibold">🛒 Custom Web Applications</summary>
+      <div class="collapse-content text-sm">
+        <ul class="list rounded-box shadow-md">
+          <li class="list-row">Marketplaces, dashboards, admin panels</li>
+          <li class="list-row">Real-time systems</li>
+          <li class="list-row">Role-based systems (Admin/User/Seller)</li>
+        </ul>
+      </div>
+    </details>
     </div>
 </template>
 <script setup>
